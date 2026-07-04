f542accc4db7bdd70229f99f010053088a14ae8c
phong2711 <145029451+phong2711@users.noreply.github.com>
2026-07-04 15:34:14 +0200

    init backend

---
+++ shop-app-backend/src/test/java/com/example/shop_app_backend/ShopAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
